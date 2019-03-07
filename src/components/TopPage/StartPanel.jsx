99645756e0ec53ee47fb891422c9de0fee06d61d
Keiichiro Ono <keiono@gmail.com>
2019-03-06 17:39:07 -0800

    URL query parser added.

--- src/components/TopPage/StartPanel.jsx
+++ src/components/TopPage/StartPanel.jsx
@@ -7,10 +7,24 @@ import ndex from '../../assets/images/ndex-logo.svg'
 import LoadingPanel from '../LoadingPanel'
 import SourcePanel from './SourcePanel'
 
-const StartPanel = props => {
+import queryString from 'query-string'
 
+
+const StartPanel = props => {
   useEffect(() => {
-    console.log('Checking available remote service')
+
+
+    const params = queryString.parse(props.location.search)
+    console.log('**MOUNTED: path params:', params)
+
+    const genes = params.genes
+
+    if(genes !== undefined) {
+      const geneNames = genes.split(',')
+      console.log('Got genes.  Start search now!', geneNames)
+      //TODO: call actual API
+    }
+
     props.sourceActions.findSourceStarted()
     return () => {
       console.log('Page unmounted')
