64c44600eba9fdb6a8d9a515eb195582d7892298
Keiichiro Ono <keiono@gmail.com>
2019-07-02 17:53:13 -0700

    UD-451 Search function added to the text area

--- src/components/AppShell/GeneTextBox.jsx
+++ src/components/AppShell/GeneTextBox.jsx
@@ -9,7 +9,7 @@ import Icon from '@material-ui/core/Icon'
 import Tooltip from '@material-ui/core/Tooltip'
 import classNames from 'classnames'
 import IconButton from '@material-ui/core/IconButton'
-import InputLabel from '@material-ui/core/InputLabel'
+import SearchIcon from '@material-ui/icons/Search'
 
 const styles = {
   root: {
@@ -41,6 +41,8 @@ const GeneTextBox = props => {
   const { classes } = props
   const geneTextRef = useRef(null)
 
+  const [queryText, setQuery] = useState(props.search.queryGenes)
+
   useEffect(() => {
     loadCSS(
       'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
@@ -57,14 +59,41 @@ const GeneTextBox = props => {
     document.execCommand('copy')
   }
 
+  const handleSearch = (evt, val) => {
+    console.log('Search start:', queryText)
+
+    const genes = queryText
+    const sources = props.source.sources
+
+    if (genes.length === 0 || sources === null || sources.length === 0) {
+      // TODO: add better error message
+      return
+    }
+
+    const sourceNames = sources.map(source => source.name)
+
+    const geneListString = genes.replace(',', ' ')
+    const geneList = geneListString.split(/ /)
+
+    props.searchActions.clearAll()
+    props.history.push('/')
+    props.searchActions.setQuery(geneListString)
+    props.searchActions.searchStarted({ geneList, sourceNames })
+  }
+
+  const handleChange = (evt) => {
+    const value = evt.target.value
+    setQuery(value)
+  }
+
   return (
     <Paper className={classes.root} elevation={0}>
       <InputBase
         id={ORIGINAL_GENE_TEXT}
         className={classes.input}
         placeholder="Genes entered"
-        value={props.search.queryGenes}
-        readOnly
+        value={queryText}
+        onChange={handleChange}
         ref={geneTextRef}
       />
       <Divider className={classes.divider} />
@@ -76,6 +105,19 @@ const GeneTextBox = props => {
           onClick={handleCopy}
         >
           <Icon className={classNames(classes.icon, 'far fa-clipboard')} />
+        </IconButton>
+      </Tooltip>
+
+      <Divider className={classes.divider} />
+
+      <Tooltip title="Start new search" placement="bottom">
+        <IconButton
+          color="primary"
+          className={classes.iconButton}
+          aria-label="Directions"
+          onClick={handleSearch}
+        >
+          <SearchIcon />
         </IconButton>
       </Tooltip>
     </Paper>
