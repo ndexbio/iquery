a7487cbe5eebede87de74c0db34980708e7362a9
D Otasek <dotasek.dev@gmail.com>
2019-07-09 15:50:35 -0700

    Initial component import; breaking build

---
+++ src/components/NDExSave/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import { DialogContentText, DialogActions, Button } from '@material-ui/core'
+
+import config from './assets/config'
+import './style.css'
+
+export default class NDExSave extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      networkUrl: ''
+    }
+  }
+
+  saveToNDEx = () => {
+    const { profile, cx } = this.props
+    console.log(profile)
+
+    const authorization = profile.authorization.token
+
+    axios
+      .post(config.save_to_ndex, cx, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: authorization
+        }
+      })
+      .then(resp => {
+        const networkUrl = resp.data.replace('/v2/', '/#/')
+        this.setState({ networkUrl })
+      })
+      .catch(err => {
+        alert('Failed to save network to NDEx: ' + err)
+      })
+  }
+
+  handleClose = () => {
+    this.props.handleClose()
+  }
+
+  render() {
+    const { networkUrl } = this.state
+    const { profile } = this.props
+
+    return (
+      profile && (
+        <div className="ndex-save">
+          <DialogContentText>
+            Now that you're logged in, you can save the network to your NDEx
+            account
+          </DialogContentText>
+          <DialogActions>
+            {networkUrl ? (
+              <Button href={networkUrl} target="_blank">
+                Open in NDEx
+              </Button>
+            ) : (
+              <Button onClick={this.saveToNDEx}>Save to my account</Button>
+            )}
+          </DialogActions>
+        </div>
+      )
+    )
+  }
+}
