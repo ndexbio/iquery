455e3ffabd6fee5cbdf3242b9ec282b329ffe568
D Otasek <dotasek.dev@gmail.com>
2019-07-15 10:27:29 -0700

    Fixes UD-449

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -2,6 +2,6 @@ export const METHOD_POST = 'POST'
 export const METHOD_GET = 'GET'
 
 // Primary
-export const BASE_URL = 'http://test.ndexbio.org/integratedsearch/'
-//export const BASE_URL = 'http://dev.ndexbio.org:8290/'
+//export const BASE_URL = 'http://test.ndexbio.org/integratedsearch/'
+export const BASE_URL = 'http://dev.ndexbio.org:8290/'
 // export const BASE_URL = 'http://secret.ndexbio.org:9080/'
