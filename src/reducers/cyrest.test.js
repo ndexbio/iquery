f74c72be406b1e88c70e71beb8b671d09c1b9836
D <dotasek.dev@gmail.com>
2019-07-22 13:45:39 -0700

    First cyrest tests

---
+++ src/reducers/cyrest.test.js
@@ -0,0 +1,27 @@
+import cyrestReducer from './cyrest.js'
+import { SET_PORT } from '../actions/cyrest.js'
+
+it('initial state', () => {
+  const action = { type: 'dummy_action' }
+  const initialState = {
+    available: false,
+    error: null,
+    isFetchingAvailable: false,
+    isPollingAvailable: false,
+    port: 1234
+  }
+
+  expect(cyrestReducer(undefined, action)).toEqual(initialState)
+})
+
+it('SET_PORT', () => {
+  const action = { type: SET_PORT, payload: 1235 }
+  const state = {
+    available: false,
+    error: null,
+    isFetchingAvailable: false,
+    isPollingAvailable: false,
+    port: 1235
+  }
+  expect(cyrestReducer(undefined, action)).toEqual(state)
+})
