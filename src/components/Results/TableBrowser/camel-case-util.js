59a13f59feaff60bf0dbcfef4a8bfa30d7fc72cf
ceofy <sol015@ucsd.edu>
2019-08-13 14:58:30 -0700

    Fixed capitalization error

--- src/components/Results/TableBrowser/camel-case-util.js
+++ src/components/Results/TableBrowser/camel-case-util.js
@@ -1,5 +1,8 @@
-export const camelCaseToTitleCase = (in_camelCaseString) => {
-    var result = in_camelCaseString                         
+export const camelCaseToTitleCase = (camelCaseString) => {
+    if (camelCaseString.slice(0, 4) === "http") {
+        return camelCaseString
+    }
+    let result = camelCaseString
         .replace(/([a-z])([A-Z][a-z])/g, "$1 $2")           
         .replace(/([A-Z][a-z])([A-Z])/g, "$1 $2")           
         .replace(/([a-z])([A-Z]+[a-z])/g, "$1 $2")          
@@ -7,8 +10,9 @@ export const camelCaseToTitleCase = (in_camelCaseString) => {
         .replace(/([a-z]+)([A-Z0-9]+)/g, "$1 $2")    
         .replace(/([A-Z]+)([A-Z][a-rt-z][a-z]*)/g, "$1 $2")
         .replace(/([0-9])([A-Z][a-z]+)/g, "$1 $2") 
-        .replace(/([A-Z]{2,})([0-9]{2,})/g, "$1 $2")       
         .replace(/([0-9]{2,})([A-Z]{2,})/g, "$1 $2")      
         .trim();
-    return result.charAt(0).toUpperCase() + result.slice(1);
+    result = result.charAt(0).toUpperCase() + result.slice(1)
+    result = result.replace('Wiki Pathways', 'WikiPathways')
+    return result
   }
