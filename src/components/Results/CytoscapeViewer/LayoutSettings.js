8d4b6c313e5133768f64fa3130f55026dda1e060
Keiichiro Ono <kono@ucsd.edu>
2019-09-13 14:22:54 -0700

    Constans are extracted to separate file from CytoscapeViewer

---
+++ src/components/Results/CytoscapeViewer/LayoutSettings.js
@@ -0,0 +1,26 @@
+export const PRESET_LAYOUT = {
+  name: 'preset',
+  padding: 6,
+  animate: false
+}
+
+export const CONCENTRIC_LAYOUT = {
+  name: 'concentric',
+  padding: 6,
+  minNodeSpacing: 100,
+  animate: false
+}
+
+export const COSE_LAYOUT = {
+  name: 'cose',
+  padding: 6,
+  nodeRepulsion: function(node) {
+    return 10080000
+  },
+  nodeOverlap: 400000,
+  idealEdgeLength: function(edge) {
+    return 10
+  },
+  animate: false,
+  fit: true
+}
