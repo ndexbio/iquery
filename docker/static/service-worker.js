33644f4903586987c574ffa9673077215e289d7c
ceofy <sol015@ucsd.edu>
2019-11-22 17:45:27 -0800

    Forgot to build

--- docker/static/service-worker.js
+++ docker/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4dd7c1463c0d32a42099fe37114b330.js"
+  "/precache-manifest.5276cc60a3680e747bf48cf9270f73e7.js"
 );
 
 self.addEventListener('message', (event) => {
