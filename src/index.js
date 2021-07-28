aa1049d83d78cd8056a4f66baeac1a811e1eec00
Chris Churas <churas.camera@gmail.com>
2021-07-28 15:26:54 -0700

    added /iquery and probably redundant {process.env.PUBLIC_URL}  exact paths so that a request to xxx.org/iquery is not matched as a job, but instead goes to the top page

--- src/index.js
+++ src/index.js
@@ -87,6 +87,8 @@ const Root = ({ store }) => (
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Top} />
+        <Route exact path="/iquery" component={Top} />
+        <Route exact path={process.env.PUBLIC_URL}  component={Top} />
         <Route path="/:jobid/:sourceId/:networkId" component={App} />
         <Route path="/:jobid/:sourceId" component={App} />
         <Route path="/:jobid" component={App} />
