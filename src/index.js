0194591763430a2be6784ab081ac3ce5c7ebb4bf
Chris Churas <churas.camera@gmail.com>
2021-07-28 16:30:32 -0700

    added more exact paths to handle odd urls that get sent by apache when coming from different domain names

--- src/index.js
+++ src/index.js
@@ -88,6 +88,8 @@ const Root = ({ store }) => (
       <Switch>
         <Route exact path="/" component={Top} />
         <Route exact path="/iquery" component={Top} />
+        <Route exact path="/iquery/iquery" component={Top} />
+        <Route exact path="/iquery//" component={Top} />
         <Route exact path={process.env.PUBLIC_URL}  component={Top} />
         <Route path="/:jobid/:sourceId/:networkId" component={App} />
         <Route path="/:jobid/:sourceId" component={App} />
