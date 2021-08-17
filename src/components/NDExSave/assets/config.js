dfe95c4eb7f8ab7dac3e05c2d557006de45903d4
Chris Churas <churas.camera@gmail.com>
2021-08-17 15:14:57 -0700

    set NDEx url to origin since this app is now served from NDEx server

--- src/components/NDExSave/assets/config.js
+++ src/components/NDExSave/assets/config.js
@@ -1,6 +1,6 @@
 import { SERVICE_SERVER_URL } from '../../../api/apiConstants'
 
 const config = {
-  save_to_ndex: SERVICE_SERVER_URL + 'v2/network'
+  save_to_ndex: SERVICE_SERVER_URL + '/network'
 }
 export default config
