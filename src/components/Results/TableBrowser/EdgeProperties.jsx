b492f94c8fc047d37f856813641a39e43da5f3e0
Dylan Fong <dylanfong.ut@gmail.com>
2022-03-22 12:17:22 -0400

    revert edge properties display

--- src/components/Results/TableBrowser/EdgeProperties.jsx
+++ src/components/Results/TableBrowser/EdgeProperties.jsx
@@ -334,27 +334,27 @@ const EdgeProperties = props => {
             )
             break
           case edgeProperties:
-            // secondaryString = 'Edge Properties'
-            // displayCol2.push(
-            //   <ListItem
-            //     key={Math.random()}
-            //     className={classes.listPadding}
-            //     disableGutters={true}
-            //   >
-            //     <ListItemText
-            //       primary={
-            //         <React.Fragment>
-            //           <Typography variant="caption" color="textSecondary">
-            //             {secondaryString}
-            //           </Typography>
-            //           <div>
-            //             <Typography variant="body2">{primaryString}</Typography>
-            //           </div>
-            //         </React.Fragment>
-            //       }
-            //     />
-            //   </ListItem>
-            // )
+            secondaryString = 'Edge Properties'
+            displayCol2.push(
+              <ListItem
+                key={Math.random()}
+                className={classes.listPadding}
+                disableGutters={true}
+              >
+                <ListItemText
+                  primary={
+                    <React.Fragment>
+                      <Typography variant="caption" color="textSecondary">
+                        {secondaryString}
+                      </Typography>
+                      <div>
+                        <Typography variant="body2">{primaryString}</Typography>
+                      </div>
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+            )
             break
         }
       }
