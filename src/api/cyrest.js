5101b9f26bb21340004d28d4281b252977053fb2
Dylan Fong <dylanfong.ut@gmail.com>
2023-01-31 17:06:51 -0500

    catch cyrest status errors when polling

--- src/api/cyrest.js
+++ src/api/cyrest.js
@@ -1,48 +1,45 @@
 import { METHOD_GET, METHOD_POST, CYREST_BASE_URL } from './config'
 
-const status = cyRESTPort => {
+const status = (cyRESTPort) => {
   const statusUrl = CYREST_BASE_URL + ':' + cyRESTPort + '/v1'
 
   return fetch(statusUrl, {
-    method: METHOD_GET
-  })
+    method: METHOD_GET,
+  }).catch(() => {})
 }
 
-const cyNDExStatus = cyRESTPort => {
-  const cyNDExStatusUrl =
-    CYREST_BASE_URL + ':' + cyRESTPort + '/cyndex2/v1/status'
+const cyNDExStatus = (cyRESTPort) => {
+  const cyNDExStatusUrl = CYREST_BASE_URL + ':' + cyRESTPort + '/cyndex2/v1/status'
 
   return fetch(cyNDExStatusUrl, {
-    method: METHOD_GET
+    method: METHOD_GET,
   })
 }
 
 const importNetwork = (cyRESTPort, payload) => {
-  const importNetworkUrl =
-    CYREST_BASE_URL + ':' + cyRESTPort + '/cyndex2/v1/networks/cx'
+  const importNetworkUrl = CYREST_BASE_URL + ':' + cyRESTPort + '/cyndex2/v1/networks/cx'
   console.log('Calling CyREST POST:', importNetworkUrl)
 
   return fetch(importNetworkUrl, {
     method: METHOD_POST,
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(payload)
+    body: JSON.stringify(payload),
   })
 }
 
 const cyndex2Networks = (cyRESTPort, method, suid, payload) => {
-  const currentNetworkUrl =
-    CYREST_BASE_URL + ':' + cyRESTPort + '/cyndex2/v1/networks/' + suid
+  const currentNetworkUrl = CYREST_BASE_URL + ':' + cyRESTPort + '/cyndex2/v1/networks/' + suid
   console.log('Calling CyREST getNetwork:', currentNetworkUrl, method, payload)
   return fetch(currentNetworkUrl, {
     method: method,
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: payload
+    body: payload,
   })
 }
 
