c0cfdddb683b31182115813ec5c60928cfca33f9
Dylan Fong <dylanfong.ut@gmail.com>
2024-10-30 11:09:19 -0400

    Add a email verification dialog box when a user has an unverified email before letting them save a network to their NDEx account.

--- src/components/Results/Ndex/SaveToNDExButton.jsx
+++ src/components/Results/Ndex/SaveToNDExButton.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import './style.css'
 import Keycloak from 'keycloak-js'
 
@@ -7,8 +7,10 @@ import SaveIcon from '@material-ui/icons/Save'
 import { withStyles } from '@material-ui/core'
 import Tooltip from '@material-ui/core/Tooltip'
 
-import { KEYCLOAK_CONFIG } from '../../../api/config'
-import { getUserProfileInformation, LOGGED_OUT_USER_PROFILE } from '../../../authentication'
+import { KEYCLOAK_CONFIG, SERVICE_SERVER_URL } from '../../../api/config'
+import { AUTH_TYPE, getUserProfileInformation, LOGGED_OUT_USER_PROFILE } from '../../../authentication'
+import { NDEx } from '@js4cytoscape/ndex-client'
+import EmailVerificationModal from './EmailVerification'
 
 const styles = (theme) => ({
   buttonIcon: {
@@ -38,6 +40,20 @@ const BootstrapButton = withStyles({
 
 const SaveToNDExButton = (props) => {
   const { classes } = props
+  const [showVerificationModal, setShowVerificationModal] = React.useState(false)
+
+  let isAuthenticated = false
+  let emailUnverified = false
+  let userName = ''
+  let userEmail = ''
+
+  const handleVerify = async () => {
+    await props.ndexSave.keycloak.loadUserProfile()
+    window.location.reload()
+  }
+  const handleCancel = () => {
+    props.ndexSave.keycloak.logout()
+  }
 
   const initializeAuthentication = () => {
     const keycloak = new Keycloak(KEYCLOAK_CONFIG)
@@ -46,8 +62,14 @@ const SaveToNDExButton = (props) => {
         onLoad: 'check-sso',
         silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
       })
-      .then((authenticated) => {
+      .then(async (authenticated) => {
+        isAuthenticated = authenticated
+        emailUnverified = true
+        userName = ''
+        userEmail = ''
         if (authenticated) {
+          const ndex = new NDEx(SERVICE_SERVER_URL)
+          await ndex.signInFromIdToken(keycloak.token)
           const userProfileInfo = getUserProfileInformation(keycloak)
           props.ndexSaveActions.setProfile(userProfileInfo)
           props.ndexSaveActions.setKeycloak(keycloak)
@@ -57,20 +79,38 @@ const SaveToNDExButton = (props) => {
         }
       })
       .catch((err) => {
-        props.ndexSaveActions.setProfile(LOGGED_OUT_USER_PROFILE)
-        props.ndexSaveActions.setKeycloak(keycloak)
-        console.log(err)
+        const errorCode = err?.response?.data?.errorCode ?? ''
+        if (errorCode === 'NDEx_User_Account_Not_Verified') {
+          emailUnverified = true
+          keycloak.loadUserInfo().then((userInfo) => {
+            userName = userInfo.name
+            userEmail = userInfo.email
+            const userProfileInfo = getUserProfileInformation(keycloak)
+            props.ndexSaveActions.setProfile(userProfileInfo)
+            props.ndexSaveActions.setKeycloak(keycloak)
+            setShowVerificationModal(true)
+          })
+        } else {
+          props.ndexSaveActions.setProfile(LOGGED_OUT_USER_PROFILE)
+          props.ndexSaveActions.setKeycloak(keycloak)
+        }
       })
   }
 
   useEffect(initializeAuthentication, [])
 
-  const handleSaveNetworkToNDEx = () => {
-    props.ndexSaveActions.setNDExModalOpen(true)
-  }
-
   const disabled = !(props.network.uuid && props.network.uuid.length > 0)
 
+  const handleSaveClick = useCallback(() => {
+    const userProfileInfo = getUserProfileInformation(props.ndexSave.keycloak)
+    if (userProfileInfo.type === AUTH_TYPE.KEYCLOAK) {
+      props.ndexSaveActions.setNDExModalOpen(true)
+    } else {
+      if (props.ndexSave.keycloak != null) {
+        props.ndexSave.keycloak.login()
+      }
+    }
+  }, [props.ndexSave.keycloak, props.ndexSaveActions])
   return (
     <Tooltip title="Save this network to your NDEx account" placement="bottom">
       <div>
@@ -78,10 +118,17 @@ const SaveToNDExButton = (props) => {
           className={classes.button}
           variant="outlined"
           disabled={disabled}
-          onClick={handleSaveNetworkToNDEx}
+          onClick={() => handleSaveClick()}
         >
           <SaveIcon color={disabled ? 'disabled' : 'inherit'} fontSize="large" className={classes.buttonIcon} />
         </BootstrapButton>
+        <EmailVerificationModal
+          open={showVerificationModal}
+          onVerify={handleVerify}
+          onCancel={handleCancel}
+          userName={userName}
+          userEmail={userEmail}
+        />
       </div>
     </Tooltip>
   )
